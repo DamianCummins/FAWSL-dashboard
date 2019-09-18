7b522d5eadee82fcc97127d4f051fc615db6f3fa
DCummin3 <dcummin3@uk.ibm.com>
2019-09-18 15:37:36 +0100

    Updates

---
+++ precache-manifest.e56ed9d0c55754c056f619b9343b88e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa61d3c89477a3bd2daf55a48dee796f",
+    "url": "/FAWSL-dashboard/index.html"
+  },
+  {
+    "revision": "ba9b6d734498bcec3010",
+    "url": "/FAWSL-dashboard/static/css/main.787b6eda.chunk.css"
+  },
+  {
+    "revision": "99c36d7c21abe2f6e66b",
+    "url": "/FAWSL-dashboard/static/js/2.e5e047ce.chunk.js"
+  },
+  {
+    "revision": "ba9b6d734498bcec3010",
+    "url": "/FAWSL-dashboard/static/js/main.62355e62.chunk.js"
+  },
+  {
+    "revision": "030cbd12c4fff13fd0d7",
+    "url": "/FAWSL-dashboard/static/js/runtime~main.1fa50346.js"
+  }
+]);
